3bc6402665b60aeebcccc941ee86d94b5eab14ef
Eko Supriyono <esup0001@gmail.com>
2024-12-18 22:00:19 +0700

    move secret to .env file

--- src/playground/settings.py
+++ src/playground/settings.py
@@ -8,10 +8,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET', '').strip()
+SECRET_KEY = os.getenv('SECRET_KEY', '').strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
 AUTH_USER_MODEL = 'user.User'
 
